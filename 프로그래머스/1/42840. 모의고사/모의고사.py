0211be8f64b01bd719c72cda93a10523d5977bf9
SIYEON KIM <94375740+ghsyn@users.noreply.github.com>
2024-09-04 17:27:16 +0900

    [level 1] Title: 모의고사, Time: 4.84 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -2,13 +2,12 @@ def solution(answers):
     pick = [[1,2,3,4,5], [2,1,2,3,2,4,2,5], [3,3,1,1,2,2,4,4,5,5]]
     student = {1:0, 2:0, 3:0}
     
-    for i in range(len(answers)):
-        if pick[0][i%5] == answers[i]:
+    for i, a in enumerate(answers):
+        if pick[0][i%5] == a:
             student[1] += 1
-        if pick[1][i%8] == answers[i]:
+        if pick[1][i%8] == a:
             student[2] += 1
-        if pick[2][i%10] == answers[i]:
+        if pick[2][i%10] == a:
             student[3] += 1
     
-    answer = [k for k, v in student.items() if v == max(student.values())]
-    return answer
+    return [k for k, v in student.items() if v == max(student.values())]
