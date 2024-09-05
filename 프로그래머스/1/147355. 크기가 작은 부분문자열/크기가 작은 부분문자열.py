d11d4a7ccc80b69332246b29ce4585d5540b6442
SIYEON KIM <94375740+ghsyn@users.noreply.github.com>
2024-09-05 10:02:57 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 3.88 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.py
@@ -1,13 +1,8 @@
 def solution(t, p):
-    sub = []
     answer = 0
     
-    for i in range(len(t)):
-        if i > len(t) - len(p):
-            break
-        sub.append(t[i:i+len(p)])
-    
-    for i in sub:
-        if int(i) <= int(p):
-            answer += 1
+    for i in range(len(t) - len(p) + 1):
+        if int(t[i:i+len(p)]) <= int(p):
+            answer +=1
+            
     return answer
