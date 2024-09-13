031ac55a83ffae15aa452e5fc8d257b378fc5358
SIYEON KIM <94375740+ghsyn@users.noreply.github.com>
2024-09-13 09:47:06 +0900

    [level 1] Title: 2016년, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.py
+++ 프로그래머스/1/12901. 2016년/2016년.py
@@ -1,12 +1,7 @@
 def solution(a, b):
     day = ['SUN','MON','TUE','WED','THU','FRI','SAT']
     month = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    date = 0
     
-    for i in range(a-1):
-        date += month[i]
-    date += b
-    
-    answer = day[(date+4) % 7]
+    date = sum(month[:a-1]) + b
         
-    return answer
+    return day[(date+4) % 7]
