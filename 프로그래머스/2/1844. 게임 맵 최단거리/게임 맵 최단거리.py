5b5bd63357e8e6eeb80e60961b0395bf8874cb0d
SIYEON KIM <94375740+ghsyn@users.noreply.github.com>
2024-09-20 15:05:55 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.56 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -0,0 +1,29 @@
+from collections import deque
+
+def solution(maps):
+    n = len(maps)
+    m = len(maps[0])
+    
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
+    
+    def bfs(i, j):
+        queue = deque()
+        queue.append((i, j))
+        
+        while queue:
+            i, j = queue.popleft()
+        
+            for idx in range(4):
+                nx = i + dx[idx]
+                ny = j + dy[idx]
+                
+                if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                    continue
+                
+                if maps[nx][ny] == 1:
+                    maps[nx][ny] = maps[i][j] + 1
+                    queue.append((nx, ny))
+                
+        return -1 if maps[n-1][m-1] == 1 else maps[n-1][m-1]
+    return bfs(0, 0)
