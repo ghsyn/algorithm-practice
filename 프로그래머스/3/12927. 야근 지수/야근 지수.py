96fd28605e151ef6127df16631874c4e82ad8ba5
SIYEON KIM <dev.demonstrate@gmail.com>
2025-03-12 16:29:41 +0900

    [level 3] Title: 야근 지수, Time: 428.41 ms, Memory: 9.32 MB -BaekjoonHub

---
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -0,0 +1,15 @@
+import math
+import heapq
+
+def solution(n, works):
+    heap = [-i for i in works]
+    heapq.heapify(heap)
+    
+    while n > 0 and heap[0] < 0:
+        max_val = heapq.heappop(heap)
+        heapq.heappush(heap, max_val + 1)
+        n -= 1
+        
+    answer = sum(math.pow(i, 2) for i in heap)
+        
+    return answer
