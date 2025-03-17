bfdf9772cfb63b30799ad2a01ea3576867074091
SIYEON KIM <dev.demonstrate@gmail.com>
2025-03-17 15:07:18 +0900

    [level 3] Title: 거스름돈, Time: 520.82 ms, Memory: 12.3 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.py
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.py
@@ -1,10 +1,9 @@
 def solution(n, money):
-    MOD = 1_000_000_007
-    dp = [0] * (n + 1)
+    dp = [0 for _ in range(n + 1)]
+    
     dp[0] = 1
-
-    for coin in money:
-        for i in range(coin, n + 1):
-            dp[i] = (dp[i] + dp[i - coin]) % MOD
-
+    for m in money:
+        for i in range(m, n + 1):
+            dp[i] += dp[i - m] % 1000000007
+    
     return dp[n]
