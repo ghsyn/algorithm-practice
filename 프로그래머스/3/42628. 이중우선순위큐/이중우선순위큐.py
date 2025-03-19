240ebe97d0552a0e2fcfef0c7a75e064d93ab6af
SIYEON KIM <dev.demonstrate@gmail.com>
2025-03-19 23:39:51 +0900

    [level 3] Title: 이중우선순위큐, Time: 300.12 ms, Memory: 15.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,24 @@
+import heapq
+
+def solution(operations):
+    min_heap = []
+    max_heap = []
+    
+    def pop_min():
+        node = heapq.heappop(min_heap)
+        max_heap.remove(-node)
+    def pop_max():
+        node = heapq.heappop(max_heap)
+        min_heap.remove(-node)
+    
+    for command in operations:
+        com, val = command.split()
+        val = int(val)
+        
+        if com == 'I':
+            heapq.heappush(min_heap, val)
+            heapq.heappush(max_heap, -val)
+        if com == 'D' and min_heap:
+                pop_min() if val < 0 else pop_max()
+    
+    return [max(min_heap), min(min_heap)] if min_heap else [0, 0]
