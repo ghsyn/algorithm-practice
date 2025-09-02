c98f28d77601630bd44187f4024bf6898a80046e
Siyeon Kim <dev.demonstrate@gmail.com>
2025-09-02 23:16:16 +0900

    [Bronze II] Title: 보물 찾기, Time: 136 ms, Memory: 111400 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/32068. 보물 찾기/보물 찾기.py
+++ 백준/Bronze/32068. 보물 찾기/보물 찾기.py
@@ -4,16 +4,9 @@ answer = []
 while T > 0:
     T -= 1
     L, R, S = list(map(int, input().split()))
-    
-    for x in range(1, R - L + 1):
-        if x % 2 == 0:
-            S += x - 1
-        elif x % 2 == 1:
-            S -= x - 1
 
-        if S == L or S == R:
-            answer.append(x)
-            break
+    x = 2 * (S - L) if S < ((L + R) / 2) else 2 * (R - S) - 1
+    answer.append(x + 1)
 
 for i in answer:
     print(i, end='\n')
