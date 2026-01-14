f62edc32a57b6aa44225f84d10f858639b55c204
Siyeon Kim <swanshawn715@gmail.com>
2026-01-14 22:09:27 +0900

    [Silver IV] Title: 제로, Time: 108 ms, Memory: 114584 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -1,8 +1,12 @@
-k = int(input())
-_stack = []
+import sys
+
+input = sys.stdin.readline
+
+k = int(input().strip())
+_stack = list()
 
 for _ in range(k):
-    num = int(input())
-    _stack.append(num) if num != 0 else _stack.pop()
-    
+    num = int(input().strip())
+    _stack.pop() if num == 0 else _stack.append(num)
+
 print(sum(_stack))
