fd02ed184ed24ef6339bdc90c3cba7d2b5bc5793
Siyeon Kim <swanshawn715@gmail.com>
2026-01-14 22:01:37 +0900

    [Silver IV] Title: 카드2, Time: 124 ms, Memory: 117200 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,10 +1,13 @@
+import sys
 from collections import deque
 
-n = int(input())
-dq = deque(i + 1 for i in range(n))
+input = sys.stdin.readline
 
-while len(dq) > 1:
-    dq.popleft()
-    dq.rotate(-1)
-    
-print(dq[0])
+n = int(input().strip())
+_dq = deque(i for i in range(1, n + 1))
+
+while len(_dq) > 1:
+    _dq.popleft()
+    _dq.rotate(-1)
+
+print(_dq[0])
