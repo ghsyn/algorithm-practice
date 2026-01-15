6295a32566b3d5bd58315e117d6447a57221c3f7
Siyeon Kim <swanshawn715@gmail.com>
2026-01-16 00:01:02 +0900

    [Gold V] Title: AC, Time: 200 ms, Memory: 158116 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -1,4 +1,7 @@
+import sys
 from collections import deque
+
+input = sys.stdin.readline
 
 t = int(input())
 
@@ -9,25 +12,22 @@ for _ in range(t):
     _dq = deque(_data.split(',')) if n > 0 else deque()
     r = 1
     is_error = False
-    
+
     for i in p:
         if i == "R":
             r *= -1
         elif i == "D":
-            if len(_dq) < 1:
+            if len(_dq) == 0:
                 is_error = True
                 break
-                
-            if r > 0:
-                _dq.popleft()
             else:
-                _dq.pop()
+                _dq.popleft() if r > 0 else _dq.pop()
         else:
             is_error = True
-        
+    
     if is_error:
-        print("error")
+        print('error')
     else:
-        if r < 0:
+        if r < 1:
             _dq.reverse()
         print('[' + ','.join(_dq) + ']')
