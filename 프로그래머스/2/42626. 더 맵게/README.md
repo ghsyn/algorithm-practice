fae7325cb041caa8f17e13b466c83b451a3be07c
Siyeon Kim <swanshawn715@gmail.com>
2026-01-17 17:31:37 +0900

    [level 2] Title: 더 맵게, Time: 1748.47 ms, Memory: 48.5 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=python3) 
 
 ### 성능 요약
 
-메모리: 50.8 MB, 시간: 1706.62 ms
+메모리: 48.5 MB, 시간: 1748.47 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 20일 11:25:51
+2026년 01월 17일 17:31:34
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -5,11 +5,12 @@ def solution(scoville, K):
     answer = 0
     
     while scoville[0] < K:
-        
-        if len(scoville) < 2:
+        if len(scoville) < 2 and scoville[-1] < K :
             return -1
         
-        heapq.heappush(scoville, heapq.heappop(scoville) + heapq.heappop(scoville) * 2)
+        min1 = heapq.heappop(scoville)
+        min2 = heapq.heappop(scoville)
+        heapq.heappush(scoville, min1 + min2 * 2)
         answer += 1
     
     return answer
